3dcf85b59fc7de9f66ddda6e9d27b23f9fbb85a6
Calvinbullock <calvinnbullock@gmail.com>
2023-09-17 11:06:53 -0700

    fixed transparent background

--- init.lua
+++ init.lua
@@ -150,7 +150,8 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+     vim.cmd.colorscheme 'onedark'
+     disable_background = true
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
