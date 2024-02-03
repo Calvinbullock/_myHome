919f1b918d44103bba21c3754a1a5f3ce790a452
Calvinbullock <calvinnbullock@gmail.com>
2024-02-02 20:50:02 -0700

    big refactor, lighter, removed screen, chroot, added exit_stat

--- prompt_bash.sh
+++ prompt_bash.sh
@@ -1,5 +1,6 @@
 # My bash Prompt
 # 	Taken from a reddit post that forked it from stevelosh.
+# 	look for PS1="...." in .bashrc and add source ~/.bash/prompt_bash.sh
 # 	Source - https://www.reddit.com/r/commandline/comments/zt6x9/what_are_your_favorite_custom_prompts/
 # 	Source - http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
 
@@ -11,38 +12,34 @@ export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWSTASHSTATE=true
 
 # colors
-export PS1_GREY="\[$(tput bold; tput setaf 0)\]"
-export PS1_GREEN="\[$(tput bold; tput setaf 2)\]"
-export PS1_YELLOW="\[$(tput bold; tput setaf 3)\]"
-export PS1_MAGENTA="\[$(tput bold; tput setaf 5)\]"
-export PS1_CYAN="\[$(tput bold; tput setaf 6)\]"
-export PS1_WHITE="\[$(tput bold; tput setaf 7)\]"
-export PS1_RESET="\[$(tput sgr0)\]"
+export PS1_GREY="\[$(tput bold; tput setaf 0)\]"    # background text
+export PS1_GREEN="\[$(tput bold; tput setaf 2)\]"   # not used
+export PS1_YELLOW="\[$(tput bold; tput setaf 3)\]"  # Systeam name
+export PS1_MAGENTA="\[$(tput bold; tput setaf 5)\]" # User
+export PS1_CYAN="\[$(tput bold; tput setaf 6)\]"    # git branch
+export PS1_WHITE="\[$(tput bold; tput setaf 7)\]"   # prompt colour
+export PS1_RESET="\[$(tput sgr0)\]"                 # reset
 
-export PS1_PINK="\[$(tput bold; tput setaf 216)\]"
+export PS1_PINK="\[$(tput bold; tput setaf 216)\]"  # current working directory path 
+export PS1_RED="\[$(tput setaf 1)\]"                # exit status
 
-# TODO not working.....
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
+# set PS1
+PS1="\n${PS1_MAGENTA}\u ${PS1_GREY}at${PS1_YELLOW} \h ${PS1_GREY}in${PS1_PINK} \w ${PS1_CYAN}\${GIT_INFO}\
+        \n\${EXIT_STAT}${PS1_WHITE}\$${PS1_RESET} "
 
 # function to set PS1
 function _bash_prompt(){
-    # git info
-    local GIT_INFO=$(git branch &>/dev/null && echo "${PS1_CYAN}git${PS1_WHITE}:$(__git_ps1 '%s')")
-
-    # add <esc>k<esc>\ to PS1 if screen is running
-    # see man(1) screen, section TITLES for more
-    if [[ "$TERM" == screen* ]]; then
-        local SCREEN_ESC='\[\ek\e\\\]'
+    # This check has to be the first thing in the function or the $? will check the last command 
+    #       in the script not the command propmt command
+    # sets a command exit statues
+    if [[ $? -eq 0 ]]; then
+        EXIT_STAT="✔️"  # Green "✔️" for success
     else
-        local SCREEN_ESC=''
+        EXIT_STAT="❌"  # Red "❌" for failure
     fi
 
-    # finally, set PS1
-    PS1="\n${debian_chroot:+($debian_chroot)}${PS1_MAGENTA}\u ${PS1_GREY}at${PS1_YELLOW} \h ${PS1_GREY}in${PS1_PINK} \w ${GIT_INFO}\
-        \n${SCREEN_ESC}${PS1_WHITE}\$${PS1_RESET} "
+    # git info
+    export GIT_INFO=$(git branch &>/dev/null && echo "$(__git_ps1 '%s')")
 }
 
 # call _bash_prompt() each time the prompt is refreshed
