4ba5084e7f135ee0afd9234e2895af8021a600b6
Calvinbullock <calvinnbullock@gmail.com>
2024-03-04 10:49:39 -0700

    moved alias and prompt files

---
+++ alias_bash.sh
@@ -0,0 +1,62 @@
+# All my bash aliases
+
+# Custom system update alias
+alias upd="sudo apt update && sudo apt upgrade && flatpak update && sudo snap refresh"
+
+# Tempature sensors
+alias temps="watch sensors"
+
+# MyHome alias
+alias basha="nvim ~/._myHome/alias_bash.sh"
+alias alac="nvim ~/._myHome/.config/alacritty/alacritty.yml"
+alias homed="cd ~/._myHome"
+
+# BASHRC and shell 
+alias bashrc="nvim ~/.bashrc"
+alias bso="source ~/.bashrc"
+alias E="exit"
+alias c="clear"
+alias l="clear"
+
+# git
+alias gc="git commit"
+alias gp="git push"
+alias gd="git diff"
+alias gitstat="git status"
+
+# VIM / NEOVIM
+alias nim="nvim"
+alias vimd="cd ~/.vim/"
+alias vimrc="vim ~/.vim/vimrc"
+alias nimd="cd ~/.config/nvim"
+alias nimrc="nvim ~/.config/nvim/init.lua"
+
+# Universial directory movement
+alias ..="cd .."
+alias ...="cd ../.."
+
+# Document folder aliaes
+alias doc="cd ~/Documents"
+alias note="nvim ~/Documents/note.txt"
+alias inscript="cd ~/Documents/install-scripts/"
+
+alias py="python3"
+
+#******** School winter 2024 alieas
+alias winter24="cd ~/Documents/_winter-2024-school-docs/"
+alias data="cd ~/Documents/_winter-2024-school-docs/dataStruc/"
+alias ap="cd ~/Documents/_winter-2024-school-docs/appliedPrograming/"
+alias pac="cd ~/Documents/_winter-2024-school-docs/cse-251-student-version/"
+alias mod="cd ~/Documents/_winter-2024-school-docs/modularDesign/"
+alias venv="source ~/Documents/_winter-2024-school-docs/cse-251-student-version/.venv/bin/activate"
+alias djvenv="source ~/Documents/_winter-2024-school-docs/appliedPrograming/.venv/bin/activate"
+alias lsnote="nvim ~/Documents/_winter-2024-school-docs/lifeCycles/lifeCyclesNotes.txt"
+
+# Tmux
+alias tmuxrc="nvim ~/.tmux.conf"
+alias ta="tmux a -t"
+alias tl="tmux ls"
+alias tk="tmux kill-session -t"
+alias tm="tmux new -s"
+alias tso="tmux source-file ~/.tmux.conf"
+
