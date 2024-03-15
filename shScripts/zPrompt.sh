77761a7b259028eddbb177d5b77f175681f300b8
Calvinbullock <calvinnbullock@gmail.com>
2024-03-15 14:16:32 -0600

    zsh prompt git branch - not status yet..

--- shScripts/zPrompt.sh
+++ shScripts/zPrompt.sh
@@ -1,19 +1,14 @@
 # Zsh Prompt based on your Bash Prompt
 #
 # Git Info
-export GIT_PS1_SHOWDIRTYSTATE=true
-export GIT_PS1_SHOWUNTRACKEDFILES=true
-export GIT_PS1_SHOWSTASHSTATE=true
-
-# Prompt function
-function _zsh_prompt() {
-  # Get git branch info
-  GIT_INFO=$(git branch &>/dev/null && echo "$(parse_git_branch %s)")
+function parse_git_branch() {
+    git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/[\1]/p'
 }
+setopt PROMPT_SUBST
 
 # Build the prompt string
 NEWLINE=$'\n'
-PS1="${NEWLINE}%B%{$fg[magenta]%}%{$fg[magenta]%}%n%{$fg[grey]%} @ %{$fg[yellow]%}%M %{$fg[green]%}%~ ${NEWLINE}%(?.%{$fg[green]%}->.%{$fg[red]%}-> )%{$reset_color%} "
+PS1="${NEWLINE}%B%{$fg[magenta]%}%{$fg[magenta]%}%n%{$fg[grey]%} @ %{$fg[yellow]%}%M %{$fg[grey]%}in %{$fg[green]%}%~ %{$fg[blue]%}$(parse_git_branch)${NEWLINE}%(?.%{$fg[green]%}->.%{$fg[red]%}-> )%{$reset_color%} "
 
 # Call prompt function before displaying prompt
 precmd_functions+=(_zsh_prompt)
