be06ed48fb99b167e5f39c4c5eb28bf073b1d874
Calvinbullock <calvinnbullock@gmail.com>
2024-03-19 10:19:44 -0600

    set grep ls and others to have colours aliases
    pulled the colours out of the defult bashrc into ,y shared alias file

--- shScripts/aliases.sh
+++ shScripts/aliases.sh
@@ -27,6 +27,14 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
+alias ls='ls --color=auto'
+#alias dir='dir --color=auto'
+#alias vdir='vdir --color=auto'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
 # git
 alias gc="git commit"
 alias gps="git push"
