7e9b1c443fe1c9bac11fc6f24212779035581ad1
Calvinbullock <calvinnbullock@gmail.com>
2024-03-21 20:10:52 -0600

    updates to install script

--- shScripts/installs.sh
+++ shScripts/installs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+# A script to install and set up my programs and configs, at least the ones that are avalable in a pkg manager.
 
-# update mirrirs and system for apt based systems
+# update mirrirs and system
 echo "update with apt"
 sudo apt update && sudo apt upgrade
-
 
 # ####### Package installs ####### #
 
@@ -16,17 +16,16 @@ if [[ $yn =~ ^[Yy]([Ee][Ss])?$ ]]; then
     sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 fi
 
-
 echo
-echo "flatpak install krita, flatseal, discord, upscaler"
+#echo "flatpak install krita, flatseal, discord, upscaler"
+echo "flatpak install flatseal, upscaler"
 read -p "Do you want to install listed items? (yes/no) " yn
 if [[ $yn =~ ^[Yy]([Ee][Ss])?$ ]]; then
     sudo flatpak install com.github.tchx84.Flatseal
+    sudo flatpak install io.gitlab.theevilskeleton.Upscaler
     # sudo flatpak install org.kde.krita
     # sudo flatpak install com.discordapp.Discord
-    # sudo flatpak install io.gitlab.theevilskeleton.Upscaler
 fi
-
 
 echo
 echo "install nvim snap"
@@ -35,9 +34,8 @@ if [[ $yn =~ ^[Yy]([Ee][Ss])?$ ]]; then
     sudo snap install neovim
 fi
 
-
 echo
-echo "apt install zsh, btop, ncdu, alacritty, tmux, ranger, lm-sensers"
+echo "apt install zsh, btop, ncdu, alacritty, tmux, ranger, lm-sensers, btop"
 read -p "Do you want to install listed items? (yes/no) " yn
 if [[ $yn =~ ^[Yy]([Ee][Ss])?$ ]]; then
     sudo apt install btop
@@ -48,11 +46,11 @@ if [[ $yn =~ ^[Yy]([Ee][Ss])?$ ]]; then
     sudo apt install alacritty
     sudo apt install tmux
     sudo apt install ranger
+    sudo apt install btop
 fi
 
-
 echo
-echo "link configs, tmux, nvim, alacritty, ranger, bashrc"
+echo "sym link configs, tmux, nvim, alacritty, ranger, bashrc. Change sh to zsh"
 read -p "Do you want link configs? (yes/no) " yn
 if [[ $yn =~ ^[Yy]([Ee][Ss])?$ ]]; then
     # Set up my dev repo
@@ -65,8 +63,8 @@ if [[ $yn =~ ^[Yy]([Ee][Ss])?$ ]]; then
     ln -s ~/._myHome/.config/nvim ~/.config/nvim
     ln -s ~/._myHome/.config/ranger ~/.config/ranger
     # ln -s ~/_myHome/.vim ~/.vim
+    sudo chsh -s /bin/zsh 
 fi
-
 
 echo
 echo "** Manuel install needed **"
