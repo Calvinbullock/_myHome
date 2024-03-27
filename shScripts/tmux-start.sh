235813ee3b8fd9d077ba6118d8ec505a70bed720
Calvinbullock <calvinnbullock@gmail.com>
2024-03-27 10:44:55 -0600

    commenting fixes tmux-start

--- shScripts/tmux-start.sh
+++ shScripts/tmux-start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# A script to open x tmux sessions you want in any directory you like.
-# TODO makes panes and windows more configurable
+# A script to open N tmux sessions you want in any directory you like.
+# TODO makes panes and windows in sessions more configurable
 
 # Create a new tmux session with two panes
 newTmuxSesstion () {
     # function paramiters
     local sessionName="$1"      # name of the new tmux session
-    local path="$2"             # path to the directory you want to tmux to start in
+    local path="$2"             # path to the directory you want tmux to start in
 
     # Check if directory  along $path exists then change to that directory
     if [[ ! -d "$path" ]]; then
