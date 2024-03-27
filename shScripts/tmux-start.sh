65afa6f0cc3f5e77236452081ad51a869846fc71
Calvinbullock <calvinnbullock@gmail.com>
2024-03-27 11:10:09 -0600

    tmux-start update
    added prcommand ability

--- shScripts/tmux-start.sh
+++ shScripts/tmux-start.sh
@@ -7,20 +7,26 @@
 newTmuxSesstion () {
     # function paramiters
     local sessionName="$1"      # name of the new tmux session
-    local path="$2"             # path to the directory you want tmux to start in
+    local sessionDir="$2"       # path to the directory you want tmux to start in
+    local preCMD="$3"           # command to run in main pane
 
     # Check if directory  along $path exists then change to that directory
-    if [[ ! -d "$path" ]]; then
-        echo "Error: Directory '$path' does not exist."
+    if [[ ! -d "$sessionDir" ]]; then
+        echo "Error: Directory '$sessionDir' does not exist."
         return 1  # Return non-zero code to indicate error
     else
-        cd "$path"
+        cd "$sessionDir"
     fi
                         
     # Create tmux session / set up 2nd pane
     tmux new -d -s $sessionName || tmux attach -t $sessionName
     tmux split-window -h -p 20
 
+    # if a preCMD is given run it in pane 1 (my panes are set to start at 1 not 0)
+    if [[ ! -z "$preCMD" ]]; then
+        tmux select-pane -t 1
+        tmux send-keys "$preCMD" Enter
+    fi
     return 0
 }
 
