14c7ce8f6c294acec9ddb92a419b7741913a358c
Calvinbullock <calvinnbullock@gmail.com>
2024-04-15 10:07:45 -0600

    small coment change

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -367,7 +367,7 @@ vim.o.termguicolors = true
 --    See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
--- Remap for dealing with word wrap
+-- Remap for dealing with word wrap -- not sure what this does yet
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
