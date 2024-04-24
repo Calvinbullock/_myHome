d4ec931f29b1500b892addd3339255a7f330af41
Calvinbullock <calvinnbullock@gmail.com>
2024-04-24 11:48:32 -0600

    updated clang

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -505,7 +505,7 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-	-- clangd = {},
+	clangd = {'cpp', 'h'},
 	-- gopls = {},
 	-- pyright = {},
 	-- rust_analyzer = {},
