e6e51f04f6bc6de3d60901f8bef955eda5f2f575
Calvinbullock <calvinnbullock@gmail.com>
2024-04-28 11:05:44 -0600

    nvim update

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -40,6 +40,17 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>y", [["+Y]])
 
+-- [[ Highlight on yank ]]
+--    See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = '*',
+})
+
 
 -- =====================================================
 --				[[ Setting options ]]				  --
