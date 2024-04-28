8cadcd128c5ff8bb7889465166d386d978686f87
Calvinbullock <calvinnbullock@gmail.com>
2024-04-28 11:05:13 -0600

    nvim git plugins update

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,8 @@
+return  {
+  {
+    'tpope/vim-fugitive',
+  },
+  {
+    'tpope/vim-rhubarb',
+  }
+}
--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,42 +1,35 @@
 return {
-  {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    config = function ()
-      require("lualine").setup({
-        opts = {
-          -- See `:help gitsigns.txt`
-          signs = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
-          },
-          on_attach = function(bufnr)
-            vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
-
-            -- don't override the built-in and fugitive keymaps
-            local gs = package.loaded.gitsigns
-            vim.keymap.set({'n', 'v'}, ']c', function()
-              if vim.wo.diff then return ']c' end
-              vim.schedule(function() gs.next_hunk() end)
-              return '<Ignore>'
-            end, {expr=true, buffer = bufnr, desc = "Jump to next hunk"})
-            vim.keymap.set({'n', 'v'}, '[c', function()
-              if vim.wo.diff then return '[c' end
-              vim.schedule(function() gs.prev_hunk() end)
-              return '<Ignore>'
-            end, {expr=true, buffer = bufnr, desc = "Jump to previous hunk"})
-          end,
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
+  config = function ()
+    require("lualine").setup({
+      opts = {
+        -- See `:help gitsigns.txt`
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
         },
-      })
-    end
-  },
-  {
-    'tpope/vim-fugitive',
-  },
-  {
-	'tpope/vim-rhubarb',
-  }
+        on_attach = function(bufnr)
+          vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+
+          -- don't override the built-in and fugitive keymaps
+          local gs = package.loaded.gitsigns
+          vim.keymap.set({'n', 'v'}, ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true, buffer = bufnr, desc = "Jump to next hunk"})
+          vim.keymap.set({'n', 'v'}, '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true, buffer = bufnr, desc = "Jump to previous hunk"})
+        end,
+      },
+    })
+  end
 }
+
