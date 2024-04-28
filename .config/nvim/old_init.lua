69226fea3fd1ab45e069a33b5a00663eb2af0981
Calvinbullock <calvinnbullock@gmail.com>
2024-04-28 15:07:31 -0600

    commented out unused exstention in old-init

--- .config/nvim/old_init.lua
+++ .config/nvim/old_init.lua
@@ -48,7 +48,7 @@ require('lazy').setup({
 	'tpope/vim-rhubarb',
 
 	-- Detect tabstop and shiftwidth automatically
-	'tpope/vim-sleuth',
+	-- 'tpope/vim-sleuth',
 
 	-- NOTE: This is where your plugins related to LSP can be installed.
 	--  The configuration is done below. Search for lspconfig to find it below.
