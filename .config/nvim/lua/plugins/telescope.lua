2bfad3c05334349ecb22fe273fee9c132e43acf7
Calvinbullock <calvinnbullock@gmail.com>
2024-05-01 13:25:26 -0600

    file fzf search space+s+f -> space+s

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -43,7 +43,7 @@ return {
         end, { desc = '[/] Fuzzily search in current buffer' })
 
         vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-        vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>s', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
         vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
         vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
