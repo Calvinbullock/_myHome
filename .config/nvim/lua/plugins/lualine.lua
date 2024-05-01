9d82c35893684cd3a3739e4fa130c40924ae7360
Calvinbullock <calvinnbullock@gmail.com>
2024-05-01 10:44:12 -0600

    nvim lualine / cmp plugin update
    cmp added ctrl+y for snipit completion inseted of enter

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,6 +6,8 @@ return {
             options = {
                 icons_enabled = false,
                 theme = "catppuccin",
+                --component_separators = '|',
+                -- section_separators = '',
             },
         })
     end,
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -35,10 +35,16 @@ return {
                 ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete {},
-                ['<CR>'] = cmp.mapping.confirm {
+
+                --[[['<CR>'] = cmp.mapping.confirm {
                     behavior = cmp.ConfirmBehavior.Replace,
                     select = true,
                 },
+                this sets code completion to enter]]
+
+                -- sets code completions to ctrl+y
+                ['<C-y>'] = cmp.mapping.confirm { select = true },
+
                 ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
