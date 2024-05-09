99d0f2b09d4edc95a6c5cf4311039670b3f7562b
Calvinbullock <calvinnbullock@gmail.com>
2024-05-09 10:51:19 -0600

    update to nvim-cmp plugin

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -36,14 +36,17 @@ return {
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete {},
 
-                --[[['<CR>'] = cmp.mapping.confirm {
+                --[[--this sets code completion to enter:
+                ['<CR>'] = cmp.mapping.confirm {
                     behavior = cmp.ConfirmBehavior.Replace,
                     select = true,
                 },
-                this sets code completion to enter]]
+                ]]
 
-                -- sets code completions to ctrl+y
+                -- sets code completions to confirmation
                 ['<C-y>'] = cmp.mapping.confirm { select = true },
+                ['<C-h>'] = cmp.mapping.confirm { select = true },
+                ['<C-l>'] = cmp.mapping.confirm { select = true },
 
                 ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
