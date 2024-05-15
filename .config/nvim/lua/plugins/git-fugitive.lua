6c90f36e0658631a6da3d69d44b0c366b6753faa
Calvinbullock <calvinnbullock@gmail.com>
2024-05-15 16:47:35 -0600

    updated nvim fugative keymapings

--- .config/nvim/lua/plugins/git-fugitive.lua
+++ .config/nvim/lua/plugins/git-fugitive.lua
@@ -2,7 +2,7 @@ return  {
     'tpope/vim-fugitive',
     config = function()
         vim.keymap.set('n', '<leader>g', ':G<CR>')
-        vim.keymap.set('n', '<leader>gc', ':G commit -m "" ')
+        vim.keymap.set('n', '<leader>gc', ':G commit -m "')
         vim.keymap.set('n', '<leader>gp', ':G push<CR>')
     end
 }
