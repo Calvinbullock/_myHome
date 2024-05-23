f0ab66b53d24dcfe5e7bb10fca6c65ea7a15c211
Calvinbullock <calvinnbullock@gmail.com>
2024-05-22 20:59:24 -0600

    update to my nvim git plugins
    moved fugative and gitsigns into one file, keybinds update

--- .config/nvim/lua/plugins/git-fugitive.lua
+++
@@ -1,8 +0,0 @@
-return  {
-    'tpope/vim-fugitive',
-    config = function()
-        vim.keymap.set('n', '<leader>g', ':G<CR>')
-        vim.keymap.set('n', '<leader>gc', ':G commit -m "')
-        vim.keymap.set('n', '<leader>gp', ':G push<CR>')
-    end
-}
