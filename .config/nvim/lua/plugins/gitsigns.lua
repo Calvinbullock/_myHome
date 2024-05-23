bfb5a3d3f9243e84b593adcca84a658a19f29f06
Calvinbullock <calvinnbullock@gmail.com>
2024-05-22 20:50:35 -0600

    gitsigns keybinds update 2, nvim

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -27,7 +27,9 @@ return {
             end,
         }
         vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
-        vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
+        vim.keymap.set('n', '<leader>gb', ':Gitsigns toggle_current_line_blame<CR>', {})
+        vim.keymap.set('n', '<leader>gs', ':Gitsigns stage_hunk <CR>', {})
+        vim.keymap.set('n', '<leader>gu', ':Gitsigns undo_stage_hunk<CR>', {})
     end,
 }
 
