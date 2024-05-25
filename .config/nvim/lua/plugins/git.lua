9f4d2cd5d446310e88c9423414e179d55ed3ea09
Calvinbullock <calvinnbullock@gmail.com>
2024-05-25 12:02:41 -0600

    update to nvim git keybinds
    jump to next hunk og: g[ -> new [g

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -15,13 +15,13 @@ return {
                 on_attach = function(bufnr)
                     -- don't override the built-in and fugitive keymaps
                     local gs = package.loaded.gitsigns
-                    vim.keymap.set({'n', 'v'}, 'g]', function()
-                        if vim.wo.diff then return 'g]' end
+                    vim.keymap.set({'n', 'v'}, ']g', function()
+                        if vim.wo.diff then return ']g' end
                         vim.schedule(function() gs.next_hunk() end)
                         return '<Ignore>'
                     end, {expr=true, buffer = bufnr, desc = "Jump to next hunk"})
-                    vim.keymap.set({'n', 'v'}, 'g[', function()
-                        if vim.wo.diff then return 'g[' end
+                    vim.keymap.set({'n', 'v'}, '[g', function()
+                        if vim.wo.diff then return '[g' end
                         vim.schedule(function() gs.prev_hunk() end)
                         return '<Ignore>'
                     end, {expr=true, buffer = bufnr, desc = "Jump to previous hunk"})
