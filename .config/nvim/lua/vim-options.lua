87ff817b5b14225ca7e14d9ca283ef6420e54f05
Calvinbullock <calvinnbullock@gmail.com>
2024-05-29 09:15:19 -0600

    nvim clean up

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -157,7 +157,7 @@ vim.o.timeoutlen = 300
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
--- NOTE: You should make sure your terminal supports this
+-- NOTE  You should make sure your terminal supports this
 vim.o.termguicolors = true
 vim.opt.termguicolors = true
 
