2cf91c54b2302d021aa491318c153b6f1aed5df3
Calvinbullock <calvinnbullock@gmail.com>
2024-06-19 13:27:13 -0600

    created a bash script for fuzzy jumping tmux sessions

---
+++ shScripts/fzf-tmux-sessions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Author: Calvin Bullock
+#
+# A script to fuzzly find and jump between tmux sessions.
+# MUST have fzf installed
+
+# Get a list of tmux sessions names
+tmux_sessions="$(tmux list-sessions | cut -d: -f1)"
+
+# Use fzf for fuzzy search
+chosen_session=$(echo "$tmux_sessions" | fzf)
+
+# Check if a session was chosen
+if [[ -n "$chosen_session" ]]; then
+  # Attach to the chosen session
+  tmux switch -t $chosen_session
+else
+  echo "No session selected."
+fi
