39be9efe548ecc513405916928cdeef7f9e674fc
Calvinbullock <calvinnbullock@gmail.com>
2024-06-20 10:13:29 -0600

    beta html nvim ftplugin file

---
+++ .config/nvim/ftplugin/html.lua
@@ -0,0 +1,18 @@
+-- Define the HTML template
+local htmlTemplate = [[
+<!DOCTYPE html>
+<html>
+<head>
+  <title>New HTML Document</title>
+</head>
+<body>
+  <h1>Welcome!</h1>
+  <p>This is a new HTML document.</p>
+</body>
+</html>
+]]
+
+-- Function to insert the template
+vim.keymap.set("n", "<leader>t", function()
+    vim.api.nvim_insert(vim.api.nvim_buf_get_curpos(0)[1], htmlTemplate)
+end, {noremap = true})
