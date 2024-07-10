496498b79588c292a1bc11e58ebd835daf770c75
Calvinbullock <calvinnbullock@gmail.com>
2024-07-10 07:07:34 -0600

    added single pane func to tmux-set

--- shScripts/tmux-start.sh
+++ shScripts/tmux-start.sh
@@ -5,6 +5,31 @@
 # A script to open N tmux sessions you want in any directory you like.
 #       Makes panes and windows in sessions more configurable
 
+# Create a new tmux session with one pane
+newTmuxSession_onePanes() {
+    # function paramiters
+    local sessionName="$1"      # name of the new tmux session
+    local sessionDir="$2"       # path to the directory you want tmux to start in
+    local preCMD="$3"           # command to run in main pane
+
+    # Check if directory  along $path exists then change to that directory
+    if [[ ! -d "$sessionDir" ]]; then
+        echo "Error: Directory '$sessionDir' does not exist."
+        return 1  # Return non-zero code to indicate error
+    else
+        cd "$sessionDir"
+    fi
+                        
+    # Create tmux session / set up 2nd pane
+    tmux new -d -s $sessionName || tmux attach -t $sessionName
+
+    # if a preCMD is given run it in pane 1 (my panes are set to start at 1 not 0)
+    if [[ ! -z "$preCMD" ]]; then
+        tmux send-keys "$preCMD" Enter
+    fi
+    return 0
+}
+
 # Create a new tmux session with two panes
 newTmuxSession_twoPanes() {
     # function paramiters
@@ -59,7 +84,7 @@ newTmuxSession_ThreePanes() {
 }
 
 # all my sessions
-newTmuxSession_twoPanes "me" "$HOME/_myHome"
+newTmuxSession_onePanes "me" "$HOME/_myHome"
 newTmuxSession_twoPanes "nel" "$HOME/Documents/new-life"
 #newTmuxSession_twoPanes "bat" "$HOME/Documents/battleship"
 #newTmuxSession_twoPanes "go" "$HOME/Documents/flip"
