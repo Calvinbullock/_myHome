5bd9769a119b61c6962453ab8fe8a27e68dd1d95
Calvinbullock <calvinnbullock@gmail.com>
2024-07-14 22:12:00 -0600

    nvim - removed fidget.nvm

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -5,10 +5,6 @@ return {
         -- Automatically install LSPs to stdpath for neovim
         { 'williamboman/mason.nvim', config = true },
         'williamboman/mason-lspconfig.nvim',
-
-        -- Useful status updates for LSP - the update little window on the bottom right
-        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-        { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
     },
     config = function ()
         -- [[ Configure LSP ]]
