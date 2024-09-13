06941138a14e5e9bffc6532c444acccb75911252
Calvinbullock <calvinnbullock@gmail.com>
2024-09-13 07:11:01 -0600

    added sand.sh to shmux layouts

---
+++ shmux/layouts/sand.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source $HOME/_myHome/shmux/./functions.sh
+
+# Set up your project with a root dir, and name
+project_root ~/Documents/sandBox/smallPobs
+session_name "sand"
+
+# first, create your session
+new_session
+
+# then, layout your session
+rename_window "code"
+split_horizontal 20%
+set_current_pane 2 # split on the right 
+
+# at the end, select the window you want first
+select_window "code"
+
+# then, attach to your session!
+#attach_to_session
