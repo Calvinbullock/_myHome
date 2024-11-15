566e3945f3488607189d29da81aff9bff5906f13
Calvinbullock <calvinnbullock@gmail.com>
2024-11-15 12:39:20 -0700

    nvim - keybinds comment update

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -4,10 +4,10 @@
 --      			  [[ Keymaps ]]
 -- =====================================================
 
--- NOTE:  not sure what these do yet
---      Keymaps for better default experience
---      See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>',      { silent = true })
+-- leader key set
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>',      { desc = 'space is only leader key', silent = true })
+
+-- j / k better behavior on wrapped lines
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
