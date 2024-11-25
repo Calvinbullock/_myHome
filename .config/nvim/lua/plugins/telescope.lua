281879fdba00bcc8b3768ab3389311da27725979
Calvinbullock <calvinnbullock@gmail.com>
2024-11-25 08:30:04 -0700

    nvim - updated keybind descriptions

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -24,10 +24,10 @@ return {
         local builtin = require("telescope.builtin")
 
         vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files,    { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>sw', builtin.grep_string,   { desc = '[S]earch current [W]ord' })
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics,   { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep,     { desc = '[f]ind with [g]rep' })
+        vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = 'fzf search current buffer' })
     end,
 }
