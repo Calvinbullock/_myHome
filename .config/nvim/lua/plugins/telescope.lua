dc12451a775d49f982b48c9e9e7bd8e4a923643e
Calvinbullock <calvinnbullock@gmail.com>
2024-11-25 08:30:47 -0700

    nvim - quick typo fix

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -23,7 +23,7 @@ return {
         })
         local builtin = require("telescope.builtin")
 
-        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Find existing buffers' })
         vim.keymap.set('n', '<leader>ff', builtin.find_files,    { desc = '[S]earch [F]iles' })
         vim.keymap.set('n', '<leader>sw', builtin.grep_string,   { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics,   { desc = '[S]earch [D]iagnostics' })
