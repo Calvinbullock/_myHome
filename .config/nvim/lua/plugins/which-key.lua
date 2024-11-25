e4ba1753a80020548c12296ff158dfe234db7452
Calvinbullock <calvinnbullock@gmail.com>
2024-11-24 17:58:37 -0700

    nvim - added whitch key

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,56 @@
+---@diagnostic disable: undefined-global
+
+return {
+    "folke/which-key.nvim",
+    event = "VimEnter",
+    delay = 1,
+
+    config = function ()
+        require("which-key").setup {
+            preset = "helix",
+            icons = {
+                mappings = false,
+                keys = {
+                    Up = '<Up> ',
+                    Down = '<Down> ',
+                   Left = '<Left> ',
+                    Right = '<Right> ',
+                    C = '<C-…> ',
+                    M = '<M-…> ',
+                    D = '<D-…> ',
+                    S = '<S-…> ',
+                    CR = '<CR> ',
+                    Esc = '<Esc> ',
+                    ScrollWheelDown = '<ScrollWheelDown> ',
+                    ScrollWheelUp = '<ScrollWheelUp> ',
+                    NL = '<NL> ',
+                    BS = '<BS> ',
+                    Space = '<Space> ',
+                    Tab = '<Tab> ',
+                    F1 = '<F1>',
+                    F2 = '<F2>',
+                    F3 = '<F3>',
+                    F4 = '<F4>',
+                    F5 = '<F5>',
+                    F6 = '<F6>',
+                    F7 = '<F7>',
+                    F8 = '<F8>',
+                    F9 = '<F9>',
+                    F10 = '<F10>',
+                    F11 = '<F11>',
+                    F12 = '<F12>',
+                },
+            }
+        }
+    end,
+
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+}
