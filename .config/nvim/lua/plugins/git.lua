c4ddf04e4fb1d196eefd2a1bc4736fbe9d23738f
Calvinbullock <calvinnbullock@gmail.com>
2024-12-04 08:01:47 -0700

    nvim - git keybinds fine tuning

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -36,21 +36,23 @@ return {
                     end, {expr=true, buffer = bufnr, desc = "Jump to previous hunk"})
                 end,
             }
-            vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<CR>',              {desc = '[G]itsigns Preview [H]unk'})
-            vim.keymap.set('n', '<leader>gs', ':Gitsigns stage_hunk <CR>',               {desc = '[G]itsigns [S]tage Hunk'})
-            vim.keymap.set('n', '<leader>gu', ':Gitsigns undo_stage_hunk<CR>',           {desc = '[G]itsigns [U]ndo Stage Hunk'})
-            vim.keymap.set('n', '<leader>gr', ':Gitsigns reset_hunk<CR>',                {desc = '[G]itsigns [R]eset Hunk'})
-            vim.keymap.set('n', '<leader>gb', ':Gitsigns toggle_current_line_blame<CR>', {desc = '[G]itsigns Line [B]lame'})
-            vim.keymap.set('n', '<leader>gd', ':Gitsigns diffthis<CR>',                  {desc = '[G]itsigns [D]iff This'})
+            vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<CR>',              { desc = '[G]itsigns Preview [H]unk'})
+            vim.keymap.set('n', '<leader>gs', ':Gitsigns stage_hunk <CR>',               { desc = '[G]itsigns [S]tage Hunk'})
+            vim.keymap.set('n', '<leader>gu', ':Gitsigns undo_stage_hunk<CR>',           { desc = '[G]itsigns [U]ndo Stage Hunk'})
+            vim.keymap.set('n', '<leader>gr', ':Gitsigns reset_hunk<CR>',                { desc = '[G]itsigns [R]eset Hunk'})
+            vim.keymap.set('n', '<leader>gb', ':Gitsigns toggle_current_line_blame<CR>', { desc = '[G]itsigns Line [B]lame'})
+            vim.keymap.set('n', '<leader>gD', ':Gitsigns diffthis<CR>',                  { desc = '[G]itsigns [D]iff This'})
         end,
     },
     {
         'tpope/vim-fugitive',
         config = function()
-            vim.keymap.set('n', '<leader>gg', ':Git<CR> :resize 100%<CR>', { desc = ':[G]it Status' })
-            vim.keymap.set('n', '<leader>gc', ':Git commit -m "',          { desc = ':[G]it [C]ommit' })
-            vim.keymap.set('n', '<leader>gP', ':Git pull<CR>',             { desc = ':[G]it [P]ull<CR>' })
-            vim.keymap.set('n', '<leader>gp', ':Git push<CR>',             { desc = ':[G]it [P]ush<CR>' })
+            vim.keymap.set('n', '<leader>gg', ':Git<CR> :resize 100%<CR>', { desc = '[G]it Status' })
+            vim.keymap.set('n', '<leader>gc', ':Git commit -m "',          { desc = '[G]it [C]ommit cmd' })
+            vim.keymap.set('n', '<leader>gC', ':Git commit<CR>',           { desc = '[G]it [C]ommit buffer' })
+            vim.keymap.set('n', '<leader>gP', ':Git push<CR>',             { desc = '[G]it [P]ush<CR>' })
+            vim.keymap.set('n', '<leader>gp', ':Git pull<CR>',             { desc = '[G]it [P]ull<CR>' })
+            vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit<CR>',          { desc = '[G]it [D]iff Split'})
         end
     }}
 
