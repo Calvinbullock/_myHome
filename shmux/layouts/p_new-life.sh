88d3bcaf5b19bb870da17beed778b524dfbf275b
Calvinbullock <calvinnbullock@gmail.com>
2024-12-21 13:08:09 -0800

    shmux - layout update

--- shmux/layouts/p_new-life.sh
+++ shmux/layouts/p_new-life.sh
@@ -12,8 +12,8 @@ new_session
 # then, layout your session
 rename_window "code"
 set_current_pane 1
-split_horizontal 80%
-set_current_pane 2 # split on the right 
+split_horizontal 90%
+set_current_pane 2 # split on the right
 
 # then, attach to your session!
 #attach_to_session
--- shmux/shmux-start.sh
+++ shmux/shmux-start.sh
@@ -8,13 +8,14 @@ path="$HOME/_myHome/shmux/layouts/"
 
 # p = personal
 source "$path/p_general.sh"
-source "$path/p_qouteCli.sh"
-source "$path/p_sand.sh"
 source "$path/p_wiki.sh"
 source "$path/p_note.sh"
-#source "$path/new-life.sh"
+
+source "$path/p_qouteCli.sh"
+source "$path/p_sand.sh"
+source "$path/new-life.sh"
 
 # s = school
-source "$path/s_inheritanceDes.sh"
-source "$path/s_srPro.sh"
-source "$path/s_webDev.sh"
+#source "$path/s_inheritanceDes.sh"
+#source "$path/s_srPro.sh"
+#source "$path/s_webDev.sh"
