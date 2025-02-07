e414d388504dfe8500123040b1c9fd2cbb3fdb64
Calvinbullock <calvinnbullock@gmail.com>
2025-02-07 14:18:47 -0700

    shmux - layout updates

--- shmux/shmux-start.sh
+++ shmux/shmux-start.sh
@@ -8,8 +8,8 @@ path="$HOME/_myHome/shmux/layouts/"
 
 # p = personal
 source "$path/p_general.sh"
-source "$path/p_wiki.sh"
-source "$path/p_note.sh"
+#source "$path/p_wiki.sh"
+#source "$path/p_note.sh"
 
 source "$path/p_sand.sh"
 source "$path/p_qouteCli.sh"
@@ -18,5 +18,4 @@ source "$path/p_new-life.sh"
 # s = school
 source "$path/s_DataStrucs.sh"
 source "$path/s_DesignPatterns.sh"
-source "$path/s_webServe.sh"
-#source "$path/s_srPro.sh"
+source "$path/s_webServ.sh"
