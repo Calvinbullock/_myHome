9197e597461b8c349bd444bcb6a3c5c57ae66ecd
Calvinbullock <calvinnbullock@gmail.com>
2025-03-31 15:02:57 -0600

    scripts - neovim build script -- updated the deb pkg name

--- shScripts/deb-scripts/neovim.sh
+++ shScripts/deb-scripts/neovim.sh
@@ -15,4 +15,4 @@ git clone https://github.com/neovim/neovim
 cd neovim
 git checkout stable
 make CMAKE_BUILD_TYPE=RelWithDebInfo
-cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux86_64.deb
