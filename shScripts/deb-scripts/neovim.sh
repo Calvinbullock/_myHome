29516adbf7912861884b0de2d33e11087209bd1a
Calvinbullock <calvinnbullock@gmail.com>
2025-06-14 22:01:12 -0700

    small change to nvim shell

--- shScripts/deb-scripts/neovim.sh
+++ shScripts/deb-scripts/neovim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Prerequisites: https://github.com/neovim/neovim/blob/master/BUILD.md#build-prerequisites
-echo "may need to update prerequisites pakages."
+printf "\e[1;32m** [may need to update prerequisites pakages.] **\e[0m\n"
 
 #Build prerequisites - NOTE may need to be update
 sudo apt-get install ninja-build gettext cmake unzip curl build-essential
@@ -11,11 +11,13 @@ mkdir -p "$HOME/Documents/build"
 cd "$HOME/Documents/build"
 
 # clone / build
+printf "\e[1;32m** [Cloneing repo] **\e[0m\n"
 git clone https://github.com/neovim/neovim
 cd neovim
 git checkout stable
 
 # build for install into $HOME/.local/bin (local user install)
+printf "\e[1;32m** [Building] **\e[0m\n"
 make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$HOME/.local
 make install
 
