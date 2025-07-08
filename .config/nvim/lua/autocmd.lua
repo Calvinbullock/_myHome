4a1c4c96fb4e78e9559a6483a4300f09f36fd378
Calvinbullock <calvinnbullock@gmail.com>
2025-07-08 11:41:13 -0700

    nvim - autocmd: no longer autosaves in lua files - moved c++ autocmd down

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -25,57 +25,27 @@ vim.api.nvim_create_autocmd("bufEnter", {
 })
 
 -- ===========================================
---  C++ Header / Source File Toggle
--- ==========================================
-vim.api.nvim_create_autocmd("Filetype", {
-    pattern = { 'cpp', 'h'},
-    group = "bufEnter",
-    callback = function()
-        vim.api.nvim_buf_create_user_command(0, 'HeaderToSource', function()
-            local newFilePath = ""
-
-            -- get current file path
-            local currentBuf = vim.api.nvim_get_current_buf()
-            local fileNamePath = vim.api.nvim_buf_get_name(currentBuf)
-
-            -- get file extension
-            local fileExtenstion = fileNamePath:match("%.([^%.]+)$")
-
-            -- check and toggle header / source extension
-            if fileExtenstion == "cpp" then
-                newFilePath = fileNamePath:sub(1, -4)
-                newFilePath = newFilePath .. "h"
-
-            elseif fileExtenstion == "h" then
-                newFilePath = fileNamePath:sub(1, -2)
-                newFilePath = newFilePath .. "cpp"
-            end
-
-            -- Use only the file name not the file path for find?
-            --local new_file_name = new_file_path:match("[^/]+$")
-
-            vim.cmd("find" .. newFilePath)
-        end, { desc = 'switch from ".h" to ".cpp" and back' })
-    end,
-})
-
--- ===========================================
 --  AUTO SAVE
 -- ==========================================
 vim.api.nvim_create_autocmd({ 'BufLeave', 'CursorHold' }, {
     group = 'autosave',
     desc = 'auto-save',
-    pattern = { '*', '-.lua'},
+    pattern = { '*' },
     callback = function(ctx)
+        -- Check if the current buffer's filetype is 'lua', if yes: return early
+      if vim.bo[ctx.buf].filetype == 'lua' then
+            return
+        end
+
         if
             vim.bo[ctx.buf].modified
             and vim.bo[ctx.buf].modifiable
             and vim.bo[ctx.buf].buftype == '' -- normal buffer
             and vim.api.nvim_buf_get_name(ctx.buf) ~= '' -- buffer is named
-            and vim.uv.fs_stat(vim.api.nvim_buf_get_name(ctx.buf)) -- buffer has an existing file backing it
+            and vim.uv.fs_stat(vim.api.nvim_buf_get_name(ctx.buf)) -- Check if the buffer has an existing file backing it.
         then
             vim.cmd.update()
-            vim.defer_fn(function() vim.api.nvim_echo({ { '' } }, false, {}) end, 2000)
+            vim.defer_fn(function() vim.api.nvim_echo({ { 'Autosaved!' } }, false, {}) end, 2000)
 
             -- -- CLEAR WHITE SPACE -- 900ms pause
             -- vim.defer_fn(function()
@@ -139,3 +109,38 @@ vim.api.nvim_create_autocmd({'BufEnter', 'BufWritePre'}, {
     end
 })
 -- ]]
+
+-- ===========================================
+--  C++ Header / Source File Toggle
+-- ==========================================
+vim.api.nvim_create_autocmd("Filetype", {
+    pattern = { 'cpp', 'h'},
+    group = "bufEnter",
+    callback = function()
+        vim.api.nvim_buf_create_user_command(0, 'HeaderToSource', function()
+            local newFilePath = ""
+
+            -- get current file path
+            local currentBuf = vim.api.nvim_get_current_buf()
+            local fileNamePath = vim.api.nvim_buf_get_name(currentBuf)
+
+            -- get file extension
+            local fileExtenstion = fileNamePath:match("%.([^%.]+)$")
+
+            -- check and toggle header / source extension
+            if fileExtenstion == "cpp" then
+                newFilePath = fileNamePath:sub(1, -4)
+                newFilePath = newFilePath .. "h"
+
+            elseif fileExtenstion == "h" then
+                newFilePath = fileNamePath:sub(1, -2)
+                newFilePath = newFilePath .. "cpp"
+            end
+
+            -- Use only the file name not the file path for find?
+            --local new_file_name = new_file_path:match("[^/]+$")
+
+            vim.cmd("find" .. newFilePath)
+        end, { desc = 'switch from ".h" to ".cpp" and back' })
+    end,
+})
