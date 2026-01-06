250df6e164b763af679bc571291d8f9f38a1dd7b
Calvinbullock <calvinnbullock@gmail.com>
2026-01-06 13:59:58 -0800

    fedora setup update

--- shScripts/fedora-setup.sh
+++ shScripts/fedora-setup.sh
@@ -1,10 +1,14 @@
-
+#!/bin/bash
 # A script to install and set up my programs and configs, at least the ones that are available in a pkg manager or script-able.
 
 # exit script if a command fails
 # set -e # NOTE: do I actually want this??
 
 # prompt and collect user choices
+echo
+printf "\e[1;32m** [Enable RPM Fusion & Install Codecs (Recommended for Steam/Media)] **\e[0m\n"
+read -p "Do you want to enable RPM Fusion and install multimedia codecs? (yes/no | y/n) " setupRPMFusion
+
 echo
 printf "\e[1;32m** [install and set up flatpak / flathub] **\e[0m\n"
 read -p "Do you want to install listed items? (yes/no | y/n) " setupFlathub
@@ -22,7 +26,7 @@ printf "\e[1;32m** [install Volta node.js manger] **\e[0m\n"
 read -p "Do you want to install listed items? (yes/no | y/n) " installVolta
 
 echo
-printf "\e[1;32m** [apt install apps: steam, vim, neofetch, npm, etc ] **\e[0m\n"
+printf "\e[1;32m** [dnf install apps: steam, vim, neofetch, npm, etc ] **\e[0m\n"
 read -p "Do you want to install listed items? (yes/no | y/n) " installDebs
 
 echo
@@ -30,184 +34,171 @@ printf "\e[1;32m** [sym link configs, tmux, nvim, alacritty, ranger, bashrc, bto
 read -p "Do you want link configs? (yes/no | y/n) " linkConfigs
 
 echo
-printf "\e[1;32m**  [Sway, rofi, and waybar, install and set up] **\e[0m\n"
-printf "\e[1;32m**  [IN MOSTLY WORKING ORDER] **\e[0m\n"
+printf "\e[1;32m**  [Sway, rofi, and waybar, install and set up] **\e[0m\n"
+printf "\e[1;32m**  [IN MOSTLY WORKING ORDER] **\e[0m\n"
 read -p "Do you want to set up sway as you Window Manager (yes/no | y/n)? " setupSway
 
 # update mirrors and system
-printf "\e[1;32m** [updateing with apt] **\e[0m\n"
-sudo apt update && sudo apt upgrade
+printf "\e[1;32m** [updateing with dnf] **\e[0m\n"
+sudo dnf update -y
 
 # ####### ################ ####### #
 # ####### Package installs ####### #
 # ####### ################ ####### #
 
-# flatpak repo installs =======================
-if [[ $setupFlathub =~ ^[Yy]([Ee][Ss])?$ ]]; then
+# RPM Fusion and Codecs =======================
+if [[ $setupRPMFusion =~ ^[Yy]([Ee][Ss])?$ ]]; then
     echo ""
     printf "\e[1;32m** {=====================================} **\e[0m\n"
-    printf "\e[1;32m**        {Installing flathub repo}        **\e[0m\n"
+    printf "\e[1;32m** {  Enabling RPM Fusion & Codecs      } **\e[0m\n"
     printf "\e[1;32m** {=====================================} **\e[0m\n"
-    sudo apt install flatpak
-    sudo "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo"
+    # Install RPM Fusion Free and Nonfree
+    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+    # Install multimedia codecs (Fedora's version of Ubuntu Restricted Extras)
+    sudo dnf groupupdate core -y
+    sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+    sudo dnf groupinstall "Multimedia" "Sound and Video" -y
+fi
+
+# flatpak repo installs =======================
+if [[ $setupFlathub =~ ^[Yy]([Ee][Ss])?$ ]]; then
+    echo ""
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    printf "\e[1;32m**        {Installing flathub repo}        **\e[0m\n"
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    sudo dnf install flatpak -y
+    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 fi
 
 # flatpak installs ============================
-if [[ $installFlatPakPkgs =~ ^[Yy]([Ee][Ss])?$ ]]; then
-    echo ""
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    printf "\e[1;32m**       {Installing flatpak apps}         **\e[0m\n"
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    flatpak install com.github.tchx84.Flatseal
-    flatpak install io.gitlab.theevilskeleton.Upscaler
-    flatpak install org.kde.krita
-    #flatpak install flathub com.brave.Browser
-    flatpak install com.discordapp.Discord
+if [[ $installFlatpakPkgs =~ ^[Yy]([Ee][Ss])?$ ]]; then
+    echo ""
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    printf "\e[1;32m**       {Installing flatpak apps}         **\e[0m\n"
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    flatpak install flathub com.github.tchx84.Flatseal -y
+    flatpak install flathub io.gitlab.theevilskeleton.Upscaler -y
+    flatpak install flathub org.kde.krita -y
+    flatpak install flathub com.discordapp.Discord -y
 fi
 
 # nvim from source installs =====================
 if [[ $buildNvim =~ ^[Yy]([Ee][Ss])?$ ]]; then
-    echo ""
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    printf "\e[1;32m**      {Installing / building nvim}       **\e[0m\n"
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    chmod +x $HOME/_myHome/shScripts/deb-scripts/neovim.sh
-    source $HOME/_myHome/shScripts/deb-scripts/neovim.sh
+    echo ""
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    printf "\e[1;32m**      {Installing / building nvim}       **\e[0m\n"
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    chmod +x $HOME/_myHome/shScripts/deb-scripts/neovim.sh
+    source $HOME/_myHome/shScripts/deb-scripts/neovim.sh
 fi
 
 # install volta (node.js version manager) =====================
 if [[ $installVolta =~ ^[Yy]([Ee][Ss])?$ ]]; then
-    echo ""
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    printf "\e[1;32m**            {Installing Volta}           **\e[0m\n"
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    curl https://get.volta.sh | bash
+    echo ""
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    printf "\e[1;32m**            {Installing Volta}           **\e[0m\n"
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    curl https://get.volta.sh | bash
 fi
 
-# apt installs ===================== basic terminal utilis / tools / apps
+# dnf installs ===================== basic terminal utilis / tools / apps
 if [[ $installDebs =~ ^[Yy]([Ee][Ss])?$ ]]; then
-    echo ""
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    printf "\e[1;32m**         {installing apt pkgs...}        **\e[0m\n"
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    echo ""
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    printf "\e[1;32m**         {installing dnf pkgs...}        **\e[0m\n"
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
 
-    printf "\e[1;32m** {terminal tools installing} **\e[0m\n"
-    # -- terminal tools I use every day
-    sudo apt install zoxide -y
-    sudo apt install zsh -y
-    sudo apt install tmux -y
-    sudo apt install ranger -y
-    sudo apt install tldr -y
-    sudo apt install bat -y
+    printf "\e[1;32m** {terminal tools installing} **\e[0m\n"
+    sudo dnf install zoxide zsh tmux ranger tldr bat -y
 
-    printf "\e[1;32m** {Basic apps} **\e[0m\n"
-    # -- apps
-    #sudo apt install kitty -y
-    sudo apt install alacritty -y
-    sudo apt install steam -y
-    #sudo apt install krita -y
+    printf "\e[1;32m** {Basic apps} **\e[0m\n"
+    sudo dnf install alacritty steam -y
 
-    printf "\e[1;32m** {nvim plugin deps / teminal tools} **\e[0m\n"
-    # -- nvim plugin dependencies
-    sudo apt install fzf -y
-    sudo apt install ripgrep -y
-    #sudo apt install npm -y
-    sudo apt install wl-clipboard
+    printf "\e[1;32m** {nvim plugin deps / teminal tools} **\e[0m\n"
+    sudo dnf install fzf ripgrep wl-clipboard -y
 
-    printf "\e[1;32m** {other programing tools} **\e[0m\n"
-    # -- other programming tools
-    sudo apt install vim -y
-    #sudo apt install gdb -y
-    #sudo apt install cgdb -y
+    printf "\e[1;32m** {other programing tools} **\e[0m\n"
+    sudo dnf install vim -y
 
-    printf "\e[1;32m** {other cli tools} **\e[0m\n"
-    # -- other cli tools
-    sudo apt install ncdu -y
-    sudo apt install btop -y
-    #sudo apt install lm-sensors -y
-    sudo apt install neofetch -y
-    #sudo apt install nvme-cli -y
-    #sudo apt install htop -y
+    printf "\e[1;32m** {other cli tools} **\e[0m\n"
+    sudo dnf install ncdu btop neofetch -y
 fi
 
 # config links =====================
 if [[ $linkConfigs =~ ^[Yy]([Ee][Ss])?$ ]]; then
-    echo ""
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    printf "\e[1;32m** {            Linking confs...         } **\e[0m\n"
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    # Set up my dev repo
+    echo ""
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    printf "\e[1;32m** {            Linking confs...         } **\e[0m\n"
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
 
-    rm "$HOME/.bashrc"
-    ln -s "$HOME/_myHome/.bashrc" "$HOME/.bashrc"
-    echo "bash conf linked"
+    rm -f "$HOME/.bashrc"
+    ln -s "$HOME/_myHome/.bashrc" "$HOME/.bashrc"
+    echo "bash conf linked"
 
-    rm "$HOME/.zshrc"
-    ln -s "$HOME/_myHome/.zshrc" "$HOME/.zshrc"
-    echo "zsh conf linked"
+    rm -f "$HOME/.zshrc"
+    ln -s "$HOME/_myHome/.zshrc" "$HOME/.zshrc"
+    echo "zsh conf linked"
 
-    rm -r "$HOME/.vim"
-    ln -s "$HOME/_myHome/.vim" "$HOME/.vim"
-    echo "vim conf linked"
+    rm -rf "$HOME/.vim"
+    ln -s "$HOME/_myHome/.vim" "$HOME/.vim"
+    echo "vim conf linked"
 
-    rm "$HOME/.tmux.conf"
-    ln -s "$HOME/_myHome/.config/tmux" "$HOME/.config/tmux"
-    echo "tmux conf linked"
+    rm -f "$HOME/.tmux.conf"
+    ln -s "$HOME/_myHome/.config/tmux" "$HOME/.config/tmux"
+    echo "tmux conf linked"
 
-    rm -r "$HOME/.config/alacritty"
-    ln -s "$HOME/_myHome/.config/alacritty" "$HOME/.config/alacritty"
-    echo "alacritty conf linked"
+    rm -rf "$HOME/.config/alacritty"
+    ln -s "$HOME/_myHome/.config/alacritty" "$HOME/.config/alacritty"
+    echo "alacritty conf linked"
 
-    rm -r "$HOME/.config/kitty"
-    ln -s "$HOME/_myHome/.config/kitty" "$HOME/.config/kitty"
-    echo "kitty conf linked"
+    rm -rf "$HOME/.config/kitty"
+    ln -s "$HOME/_myHome/.config/kitty" "$HOME/.config/kitty"
+    echo "kitty conf linked"
 
-    rm -r "$HOME/.config/nvim"
-    ln -s "$HOME/_myHome/.config/nvim" "$HOME/.config/nvim"
-    echo "nvim conf linked"
+    rm -rf "$HOME/.config/nvim"
+    ln -s "$HOME/_myHome/.config/nvim" "$HOME/.config/nvim"
+    echo "nvim conf linked"
 
-    rm -r "$HOME/.config/ranger"
-    ln -s "$HOME/_myHome/.config/ranger" "$HOME/.config/ranger"
-    echo "ranger conf linked"
+    rm -rf "$HOME/.config/ranger"
+    ln -s "$HOME/_myHome/.config/ranger" "$HOME/.config/ranger"
+    echo "ranger conf linked"
 
-    rm -r "$HOME/.config/btop"
-    ln -s "$HOME/_myHome/.config/btop" "$HOME/.config/btop"
-    echo "btop conf linked"
+    rm -rf "$HOME/.config/btop"
+    ln -s "$HOME/_myHome/.config/btop" "$HOME/.config/btop"
+    echo "btop conf linked"
 fi
 
-# sway configs ====================  WARN: not done
+# sway configs ====================  
 if [[ $setupSway =~ ^[Yy]([Ee][Ss])?$ ]]; then
-    echo ""
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
-    printf "\e[1;32m** {      setting up sway .configs...    } **\e[0m\n"
-    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    echo ""
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
+    printf "\e[1;32m** {      setting up sway .configs...    } **\e[0m\n"
+    printf "\e[1;32m** {=====================================} **\e[0m\n"
 
-    sudo sudo apt install sway waybar brightnessctl pulseaudio-utils rofi -y
+    sudo dnf install sway waybar brightnessctl pulseaudio-utils rofi -y
 
-    # set up sway config links
-    rm -r "$HOME/.config/sway"
-    ln -s "$HOME/_myHome/.config/sway" "$HOME/.config/sway"
+    rm -rf "$HOME/.config/sway"
+    ln -s "$HOME/_myHome/.config/sway" "$HOME/.config/sway"
 
-    rm -r "$HOME/.config/waybar"
-    ln -s "$HOME/_myHome/.config/waybar" "$HOME/.config/waybar"
+    rm -rf "$HOME/.config/waybar"
+    ln -s "$HOME/_myHome/.config/waybar" "$HOME/.config/waybar"
 
-    # No default config for rofi
-    ln -s "$HOME/_myHome/.config/rofi" "$HOME/.config/rofi"
+    rm -rf "$HOME/.config/rofi"
+    ln -s "$HOME/_myHome/.config/rofi" "$HOME/.config/rofi"
 fi
 
-# Auto-clean up after updates and installing everything
-printf "\e[1;32m** {Run autoclean} **\e[0m\n"
-sudo apt purge nano -y
-sudo apt autoclean
+# Auto-clean up
+printf "\e[1;32m** {Run cleanup} **\e[0m\n"
+sudo dnf remove nano -y
+sudo dnf autoremove -y
 
 echo
-printf "\e[1;32m  If you want to use zsh as defult shell, run: \e[0m\n"
-printf "\e[1;32m  Sudo chsh _USER_ -s /bin/zsh.\e[0m\n"
+printf "\e[1;32m  If you want to use zsh as defult shell, run: \e[0m\n"
+printf "\e[1;32m  sudo chsh $USER -s /bin/zsh \e[0m\n"
 echo
 printf "\e[1;32m** Manuel install needed **\e[0m\n"
-printf "\e[1;32m  Brave MineCraft, Virtbox, Chrome, vs-Code, Discord.\e[0m\n"
+printf "\e[1;32m  Brave MineCraft, Virtbox, Chrome, vs-Code, Discord.\e[0m\n"
 echo
-printf "\e[1;32m  Don't forget to set up a fire-wall!!\e[0m\n"
-printf "\e[1;32m  You can now reboot! Thanks you.\e[0m\n"
+printf "\e[1;32m  Don't forget to set up a fire-wall!!\e[0m\n"
+printf "\e[1;32m  You can now reboot! Thanks you.\e[0m\n"
 echo
-
