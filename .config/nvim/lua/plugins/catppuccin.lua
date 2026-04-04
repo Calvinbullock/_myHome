8463e548c4d579c09286b48373b0d0912ff8d75c
Calvin <your_email@example.com>
2026-04-04 16:46:14 -0700

    nvim - theme update

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -2,26 +2,52 @@
 
 return {
     "catppuccin/nvim",
-    lazy = false,
     name = "catppuccin",
+    lazy = false,
     priority = 1000,
     config = function()
+        -- 1. Setup the theme options
         require("catppuccin").setup({
-            background = {
-                dark = "mocha",
-            },
+            flavour = "mocha",
             transparent_background = true,
-            dim_inactive = {
-                enabled = false,
-                percentage = 0.0000000001,
-            },
         })
-        vim.cmd.colorscheme "catppuccin"
 
-        -- Line numbers re-color
-        function LineNumberColors()
+        -- 2. Use a "Schedule" to apply the theme.
+        -- This waits 1ms for Neovim to finish booting before applying colors.
+        vim.schedule(function()
+            vim.cmd.colorscheme "catppuccin"
+            -- Apply your pink line numbers
             vim.api.nvim_set_hl(0, 'LineNr', { fg='#faa7e7', bold=false })
-        end
-        LineNumberColors()
+        end)
     end
 }
+
+
+
+
+
+-- return {
+--     "catppuccin/nvim",
+--     lazy = false,
+--     name = "catppuccin",
+--     priority = 1000,
+--     config = function()
+--         require("catppuccin").setup({
+--             background = {
+--                 dark = "mocha",
+--             },
+--             transparent_background = true,
+--             dim_inactive = {
+--                 enabled = false,
+--                 percentage = 0.0000000001,
+--             },
+--         })
+--         vim.cmd.colorscheme "catppuccin"
+--
+--         -- Line numbers re-color
+--         function LineNumberColors()
+--             vim.api.nvim_set_hl(0, 'LineNr', { fg='#faa7e7', bold=false })
+--         end
+--         LineNumberColors()
+--     end
+-- }
